8ebe3e032d870e3fe185afbdffe86868700b35af
nyaafree <45682975+nyaafree@users.noreply.github.com>
2022-01-22 15:55:43 +0900

    feat: useCallBack,useMemoについて学習

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -10,6 +10,16 @@ export default function App() {
   const [open, setOpen] = useState(false);
   const onChangeText = (event) => setText(event.target.value);
   const onClickOpen = () => setOpen(!open);
+  // アロー関数で書いた関数というのは再レンダリングごとに新しい関数を生成しているとみなされる
+  // そのため、ChildAreaコンポーネントに渡すpropsが毎回変わっているとみなされてしまう
+  // よってpropsが変わったChildAreaコンポーネントも再描画されてしまう
+  // したがってsetOpenが生成された場合のみonClickCloseを再生成する、すなわち初回レンダリングの時にのみ第一引数のアロー関数を生成するようにすれば
+  // ChildAreaコンポーネントに渡すpropsも変わらず、不要な再レンダリングを防げる
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+  // 変数のmemo化,第二引数が空配列なので最初読み込んだ時にのみ第一引数のアロー関数が実行される
+  // 第二引数に変数が指定された場合はその変数が変わった場合のみ第一引数のアロー関数が実行される
+  const temp = useMemo(() => 1+3, []);
+  console.log(temp);
 
   return (
     <div className="App">
@@ -17,7 +27,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,19 +1,29 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
+/* memoでアロー関数部分を囲むことによってpropsが変わらないと再レンダリングしないということを宣言できる*/
+
+export const ChildArea = memo((props) => {
   console.log(style);
-  const { open } = props;
+  const { open, onClickClose } = props;
+  const data = [...Array(2000).keys()];
+  // console.log(data);
+  data.forEach(() => {
+    console.log("...");
+  });
   return (
     <>
       {open ? (
         <div style={style}>
           <p>小コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
